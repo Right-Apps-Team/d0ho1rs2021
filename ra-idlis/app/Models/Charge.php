fd2e37e726c45dd33ab82173413e4681bc634b77
schiezka <schiezkadv@gmail.com>
2021-03-08 21:59:49 +0800

    Recent changes

---
+++ ra-idlis/app/Models/Charge.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Charge extends Model
+{
+    protected $table = 'charges';
+    protected $primaryKey = 'chg_code';
+    public $incrementing = false;
+}
