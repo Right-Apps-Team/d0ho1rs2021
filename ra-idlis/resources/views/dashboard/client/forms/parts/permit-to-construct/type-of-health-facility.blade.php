cfa1d3c7416fdddc332e9635d3b3053f7215c223
schiezka <schiezkadv@gmail.com>
2021-03-11 13:48:38 +0800

    Complete Permit-to-construct html form

--- ra-idlis/resources/views/dashboard/client/forms/parts/permit-to-construct/type-of-health-facility.blade.php
+++ ra-idlis/resources/views/dashboard/client/forms/parts/permit-to-construct/type-of-health-facility.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-2 col-md-12">&nbsp;</div>
 <div class="col-md-12"><b class="text-primary">Type of Health Facility: <span class="text-danger">*</span></b></div>
-
+    
 @foreach($hfaci_service_type as $h)
 <div class="col-md-3">
     <label><input type="radio" name="hgpid" value="{{$h->hgpid}}"/> {{$h->hgpdesc}}</label>
